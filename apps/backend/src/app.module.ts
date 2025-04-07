73a29ecb47351d173e9ce957421cb8f4cab7f33a
Sanyam S <codewithsam@gmail.com>
2025-04-08 02:21:43 +0700

    setup of shadcn to create a foundation ui library of the ecom company. Use ui as a base lib of foundational elements in any other package or app. Also created some layout packages like header and left nav which can be handled by some individual teams that have their own deployment process which improves DX by having an independent sdlc

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DashboardModule } from './dashboard/dashboard.module';
+import { LayoutModule } from './layout/layout.module';
 
 @Module({
-  imports: [DashboardModule],
+  imports: [DashboardModule, LayoutModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- apps/backend/src/dashboard/dashboard.controller.ts
+++ apps/backend/src/dashboard/dashboard.controller.ts
@@ -1,13 +1,11 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Version } from '@nestjs/common';
+import { Controller, Get, Version } from '@nestjs/common';
 import { DashboardService } from './dashboard.service';
-import { CreateDashboardDto } from './dto/create-dashboard.dto';
-import { UpdateDashboardDto } from './dto/update-dashboard.dto';
 
 @Controller('dashboard')
 export class DashboardController {
   constructor(private readonly dashboardService: DashboardService) { }
 
-  @Version("1")
+  @Version('1')
   @Get()
   findAll() {
     return this.dashboardService.findAll();
